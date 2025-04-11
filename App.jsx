764cded806f8c9c358fa893ec846c5d584aa9f35
Investsure <falcongamingyt154@gmail.com>
2025-04-11 17:42:21 +0600

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import UserSignalViewer from "./components/UserSignalViewer";  // ইউজার প্যানেল কম্পোনেন্ট
+
+function App() {
+  return (
+    <div className="App">
+      <UserSignalViewer />
+    </div>
+  );
+}
+
+export default App;
